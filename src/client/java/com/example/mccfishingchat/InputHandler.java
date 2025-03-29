a1fdc5a61ef61667c2f5bcf8c91fd8ba3fc48a83
DeFlanko <deflanko@users.noreply.github.com>
2025-03-29 15:01:29 -0700

    Update README.md
    Pretty up the Readme

--- src/client/java/com/example/mccfishingchat/InputHandler.java
+++ src/client/java/com/example/mccfishingchat/InputHandler.java
@@ -8,19 +8,27 @@ import org.lwjgl.glfw.GLFW;
 
 public class InputHandler {
     private static KeyBinding toggleVisibilityKey;
+    private static KeyBinding quickNDirtyF3ToggleKey;
     
     public static void init() {
         // Register keybinding to toggle chat box visibility
         toggleVisibilityKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-            "key.mccfishingchat.toggle",
+            "MCC Fish Chatbox Toggle",
             InputUtil.Type.KEYSYM,
             GLFW.GLFW_KEY_F9,
-            "key.categories.mccfishingchat"
+            "MCC Fishing Chat"
+        ));
+
+        quickNDirtyF3ToggleKey= KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "MCC F3 Toggle",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_F3,
+                "MCC Fishing Chat"
         ));
         
         // Register mouse handlers through Fabric's event system
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (toggleVisibilityKey.wasPressed()) {
+            while (toggleVisibilityKey.wasPressed()||quickNDirtyF3ToggleKey.wasPressed()) {
                 // Toggle chat box visibility
                 MCCFishingChatMod.fishingChatBox.toggleVisibility();
             }
